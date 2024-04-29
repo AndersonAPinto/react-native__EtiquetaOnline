bfb9e857577cbee8dbadf57409f8b7f88a724e33
AndersonAPinto <andersonaugustopinto@gmail.com>
2024-04-29 12:22:30 -0300

    update msg

--- src/pages/routePolitic/routePolitic.js
+++ src/pages/routePolitic/routePolitic.js
@@ -1,23 +1,14 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { View } from 'react-native';
 // Assumindo que você também tem uma tela para isso
 
 const InfoStack = createStackNavigator();
 
 function RoutesPolitic() {
     return (
-        <InfoStack.Navigator>
-            <InfoStack.Screen
-                name="Política de Privacidade"
-                component={PoliticadePrivacidade}
-                options={{ headerShown: false }}
-            />
-            <InfoStack.Screen
-                name="Termos de Uso"
-                component={TermosdeUso}
-                options={{ headerShown: false }}
-            />
-            {/* Adicione outras telas relacionadas a informações aqui se necessário */}
-        </InfoStack.Navigator>
+        <View>
+            <div> Ícones feitos por <a href="https://www.flaticon.com/br/autores/culmbio" title="Culmbio"> Culmbio </a> from <a href="https://www.flaticon.com/br/" title="Flaticon">www.flaticon.com'</a></div>
+        </View>
     );
 }
 export default RoutesPolitic;
